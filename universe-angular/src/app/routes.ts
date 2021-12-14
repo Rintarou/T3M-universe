d0bd7e0aeebbf5d06f6614873cec35fea0bdda00
tanguy <angelliaume.tanguy@gmail.com>
2021-12-14 16:46:47 +0100

    component home visuel login inscription

--- universe-angular/src/app/routes.ts
+++ universe-angular/src/app/routes.ts
@@ -1,4 +1,5 @@
 import { InscriptionComponent } from './components/inscription/inscription.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ElementEditorComponent } from './components/element-editor/element-editor.component';
 import { ElementComponent } from './components/element/element.component';
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'home', component: InscriptionComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'inscription', component: InscriptionComponent },
   { path: 'worlds', component: ElementEditorComponent },
